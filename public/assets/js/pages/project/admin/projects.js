6f9372c29f52791888d494d00e49056475b50ba8
Rsabha107 <61208207+Rsabha107@users.noreply.github.com>
2025-02-19 12:22:05 +0300

    test

--- public/assets/js/pages/project/admin/projects.js
+++ public/assets/js/pages/project/admin/projects.js
@@ -770,51 +770,55 @@ $(document).ready(function () {
     });
 
     $("body").on("click", "#delete_project_file", function (e) {
-        var id = $(this).data("id");
-        var tableID = $(this).data("table");
-        e.preventDefault();
-        // alert("id: "+id);
-        // alert("tableID: "+tableID);
-        var link = $(this).attr("href");
-        Swal.fire({
-            title: "Are you sure?",
-            text: "Delete This Data?",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!",
-        }).then((result) => {
-            if (result.isConfirmed) {
-                $.ajax({
-                    url: "/global/files/delete/" + id,
-                    type: "DELETE",
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'), // Replace with your method of getting the CSRF token
-                    },
-                    dataType: "json",
-                    success: function (result) {
-                        if (!result["error"]) {
-                            toastr.success(result["message"]);
-                            // divToRemove.remove();
-                            // $("#fileCount").html("File ("+result["count"]+")");
-                            // console.log('before table refrest for #'+tableID);
-                            $("#" + tableID).bootstrapTable("refresh");
-                            // Swal.fire(
-                            //     'Deleted!',
-                            //     'Your file has been deleted.',
-                            //     'success'
-                            //   )
-                        }
-                    },
-                    error: function (xhr, ajaxOptions, thrownError) {
-                        console.log(xhr.status);
-                        console.log(thrownError);
-                    },
-                });
-            }
-        });
+        console.log("inside #delete_project_file");
     });
+
+    // $("body").on("click", "#delete_project_file", function (e) {
+    //     var id = $(this).data("id");
+    //     var tableID = $(this).data("table");
+    //     e.preventDefault();
+    //     // alert("id: "+id);
+    //     // alert("tableID: "+tableID);
+    //     var link = $(this).attr("href");
+    //     Swal.fire({
+    //         title: "Are you sure?",
+    //         text: "Delete This Data?",
+    //         icon: "warning",
+    //         showCancelButton: true,
+    //         confirmButtonColor: "#3085d6",
+    //         cancelButtonColor: "#d33",
+    //         confirmButtonText: "Yes, delete it!",
+    //     }).then((result) => {
+    //         if (result.isConfirmed) {
+    //             $.ajax({
+    //                 url: "/global/files/delete/" + id,
+    //                 type: "DELETE",
+    //                 headers: {
+    //                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'), // Replace with your method of getting the CSRF token
+    //                 },
+    //                 dataType: "json",
+    //                 success: function (result) {
+    //                     if (!result["error"]) {
+    //                         toastr.success(result["message"]);
+    //                         // divToRemove.remove();
+    //                         // $("#fileCount").html("File ("+result["count"]+")");
+    //                         // console.log('before table refrest for #'+tableID);
+    //                         $("#" + tableID).bootstrapTable("refresh");
+    //                         // Swal.fire(
+    //                         //     'Deleted!',
+    //                         //     'Your file has been deleted.',
+    //                         //     'success'
+    //                         //   )
+    //                     }
+    //                 },
+    //                 error: function (xhr, ajaxOptions, thrownError) {
+    //                     console.log(xhr.status);
+    //                     console.log(thrownError);
+    //                 },
+    //             });
+    //         }
+    //     });
+    // });
 
 });
 
